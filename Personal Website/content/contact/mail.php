3a879f87f6a5e410722e7cf4871c1f39398d99ff
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-11-25 12:36:59 -0800

    Update mail.php
    Changed return destination.

--- Personal Website/content/contact/mail.php
+++ Personal Website/content/contact/mail.php
@@ -8,5 +8,5 @@ $recipient = "Brayanquevedo2134@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='return.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='../../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
